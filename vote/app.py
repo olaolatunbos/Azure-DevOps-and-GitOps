3bf7c0c8812aeb9def8645a50026abf92ee217bf
Abdulquadri Olatunbosun <olaolat@hotmail.com>
2025-05-02 13:00:53 +0000

    Updated app.py

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Rain")
-option_b = os.getenv('OPTION_B', "Sun")
+option_a = os.getenv('OPTION_A', "Docker Swarm")
+option_b = os.getenv('OPTION_B', "Kubernetes")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
