2bb79b09e2813575f14cce8389e8d1ea6cde30d5
Abdulquadri Olatunbosun <olaolat@hotmail.com>
2025-05-02 12:43:39 +0000

    Updated update-manifest.sh

--- scripts/update-manifest.sh
+++ scripts/update-manifest.sh
@@ -17,7 +17,7 @@ git config user.email "build-agent@azuredevops.com"
 
 # Make changes to the Kubernetes manifest file(s)
 # For example, let's say you want to change the image tag in a deployment.yaml file
-sed -i "s|image:.*|image: votingapplication/$2:$3|g" k8s-specifications/$1-deployment.yaml
+sed -i "s|image:.*|image: votingapplication.azurecr.io/$2:$3|g" k8s-specifications/$1-deployment.yaml
 
 # Add the modified files
 git add .
